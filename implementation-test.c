25ae3a4f98655da9173cd88a3735926693e8f7ac
ArunTSetty <arun.t.shetty@gmail.com>
2016-07-08 18:09:24 -0400

    III minus I Succesful

--- implementation-test.c
+++ implementation-test.c
@@ -12,13 +12,13 @@
 #include<stdlib.h>
 #include<check.h>
 #define MAX 4096
-char ostring1[MAX] = "II";
+char ostring1[MAX] = "III";
 char ostring2[MAX] = "I";
 
 START_TEST(romancaltest)
 {
 #line 11
-	ck_assert_str_eq(subtract(ostring1,ostring2), "I");
+	ck_assert_str_eq(subtract(ostring1,ostring2), "II");
 	
 
 	
